7966f5ca3fc9e42f718f96f9307e815aebd6688e
Jarvis Luong <21980965+jarvisluong@users.noreply.github.com>
2021-08-25 10:19:52 +0300

    successful timer and found websocket api in headless task

--- example/index.js
+++ example/index.js
@@ -3,6 +3,9 @@
  */
 
 import {AppRegistry} from 'react-native';
+// We need to import the native module outside the registerHeadlessTask function to make
+// sure the react context bundle it as well during headless task execution.
+import BackgroundTimer from 'react-native-background-timer';
 import App from './App1';
 import {name as appName} from './app.json';
 
@@ -10,7 +13,10 @@ AppRegistry.registerComponent(appName, () => App);
 
 AppRegistry.registerHeadlessTask(
   'CallNotificationEventEmitter',
-  () => async (taskData) => {
+  () => async taskData => {
+    console.log('setTimeout: ', typeof BackgroundTimer.setTimeout);
+    console.log('start: ', typeof BackgroundTimer.start);
+    console.log('websocket: ', typeof WebSocket.name);
     try {
       console.log(
         'CallNotificationEventEmitter called from background service',
@@ -25,7 +31,7 @@ AppRegistry.registerHeadlessTask(
       console.log(`Error trying to run headless task: ${error.message}`);
     }
     return new Promise(resolve => {
-      setTimeout(() => {
+      BackgroundTimer.setTimeout(() => {
         console.log('timer called CallNotificationEventEmitter');
         resolve();
       }, 2000);
