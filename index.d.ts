a8cbfd0a4816cc2df4140757e85fb7261ea5c1ca
Nguyen Luong <luongnguyenkhoinguyen14@gmail.com>
2021-09-09 17:15:33 +0300

    Clean up ts file

--- index.d.ts
+++ index.d.ts
@@ -1,19 +1,10 @@
 declare module 'react-native-callkeep' {
   export type Events =
-    'didReceiveStartCallAction' |
     'answerCall' |
     'endCall' |
     'didActivateAudioSession' |
-    'didDeactivateAudioSession' |
-    'didDisplayIncomingCall' |
-    'didToggleHoldCallAction' |
-    'didPerformDTMFAction' |
-    'didResetProvider' |
-    'checkReachability' |
     'didPerformSetMutedCallAction' |
-    'didLoadWithEvents' |
-    'showIncomingCallUi' |
-    'silenceIncomingCall';
+    'didLoadWithEvents' |;
 
   type HandleType = 'generic' | 'number' | 'email';
 
@@ -73,55 +64,7 @@ declare module 'react-native-callkeep' {
 
     static removeEventListener(type: Events): void
 
-    static setup(options: IOptions): Promise<boolean>
-
-    static hasDefaultPhoneAccount(): boolean
-
-    static answerIncomingCall(uuid: string): void
-
     static fulfillAnswerCallAction(): void
-
-    static registerPhoneAccount(): void
-
-    static registerAndroidEvents(): void
-
-    static displayIncomingCall(
-      uuid: string,
-      handle: string,
-      localizedCallerName?: string,
-      handleType?: HandleType,
-      hasVideo?: boolean,
-      options?: object,
-    ): void
-
-    static startCall(
-      uuid: string,
-      handle: string,
-      contactIdentifier?: string,
-      handleType?: HandleType,
-      hasVideo?: boolean,
-    ): void
-
-    static updateDisplay(
-      uuid: string,
-      displayName: string,
-      handle: string,
-      options?: object,
-    ): void
-
-    static checkPhoneAccountEnabled(): Promise<boolean>;
-
-    static isConnectionServiceAvailable(): Promise<boolean>;
-
-    /**
-     * @description reportConnectedOutgoingCallWithUUID method is available only on iOS.
-     */
-    static reportConnectedOutgoingCallWithUUID(uuid: string): void
-
-    /**
-     * @description reportConnectedOutgoingCallWithUUID method is available only on iOS.
-     */
-    static reportConnectingOutgoingCallWithUUID(uuid: string): void
 
     static reportEndCallWithUUID(uuid: string, reason: number): void
 
@@ -131,66 +74,10 @@ declare module 'react-native-callkeep' {
 
     static fulfillEndCallAction(): void
 
-    static endAllCalls(): void
-
-    static setReachable(): void
-
-    /**
-     * @description isCallActive method is available only on iOS.
-     */
-    static isCallActive(uuid: string): Promise<boolean>
-
-    static getCalls(): Promise<object>
-
-    static getAudioRoutes(): Promise<void>
-
-    static setAudioRoute: (uuid:string, inputName: string) => Promise<void>
-
-    /**
-     * @description supportConnectionService method is available only on Android.
-     */
-    static supportConnectionService(): boolean
-
-    /**
-     * @description hasPhoneAccount method is available only on Android.
-     */
-    static hasPhoneAccount(): Promise<boolean>
-
-    static hasOutgoingCall(): Promise<boolean>
-
     /**
      * @description setMutedCall method is available only on iOS.
      */
     static setMutedCall(uuid: string, muted: boolean): void
 
-    /**
-     * @description toggleAudioRouteSpeaker method is available only on Android.
-     * @param uuid
-     * @param routeSpeaker
-     */
-    static toggleAudioRouteSpeaker(uuid: string, routeSpeaker: boolean): void
-    static setOnHold(uuid: string, held: boolean): void
-
-    /**
-     * @descriptions sendDTMF is used to send DTMF tones to the PBX.
-     */
-    static sendDTMF(uuid: string, key: string): void
-
-    static checkIfBusy(): Promise<boolean>
-
-    static checkSpeaker(): Promise<boolean>
-
-    /**
-     * @description setAvailable method is available only on Android.
-     */
-    static setAvailable(active: boolean): void
-
-    static setForegroundServiceSettings(settings: Object): void
-
-    static canMakeMultipleCalls(allow: boolean): void
-
-    static setCurrentCallActive(callUUID: string): void
-
-    static backToForeground(): void
   }
 }
