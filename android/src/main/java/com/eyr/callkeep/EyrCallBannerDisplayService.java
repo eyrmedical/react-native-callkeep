b0528af2beaa45058522898b8dab641840eed115
Jarvis Luong <21980965+jarvisluong@users.noreply.github.com>
2021-09-09 16:12:17 +0300

    Revert "correct intent to call"
    This reverts commit 64f14d553e44ee8e8f8cdf8fdbba888d867c7313.

--- android/src/main/java/com/eyr/callkeep/EyrCallBannerDisplayService.java
+++ android/src/main/java/com/eyr/callkeep/EyrCallBannerDisplayService.java
@@ -22,9 +22,6 @@ import static com.eyr.callkeep.EyrCallBannerControllerModule.*;
 public class EyrCallBannerDisplayService extends Service {
     public static final int CALL_NOTIFICATION_ID = 23;
 
-    private static final String ACCEPT_CALL = "ACCEPT_CALL";
-    private static final String OPEN_INCOMING_CALL = "OPEN_INCOMING_CALL";
-
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -52,20 +49,24 @@ public class EyrCallBannerDisplayService extends Service {
         Intent dismissBannerIntent = new Intent(this, this.getClass());
         dismissBannerIntent.setAction(DISMISS_BANNER);
 
-        Intent acceptCallIntent = new Intent(this, this.getClass());
-        acceptCallIntent.setAction(ACCEPT_CALL);
+        Intent acceptCallAndOpenApp = new Intent(this, getMainActivityClass());
+        acceptCallAndOpenApp.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+        Bundle acceptCallAndOpenAppInitialProps = new Bundle();
+        acceptCallAndOpenAppInitialProps.putBoolean("isCallAccepted", true);
+        acceptCallAndOpenApp.putExtras(acceptCallAndOpenAppInitialProps);
 
-        Intent openAppIntent = new Intent(this, this.getClass());
-        openAppIntent.setAction(OPEN_INCOMING_CALL);
+        Intent openIncomingCallScreen = new Intent(this, getMainActivityClass());
+        acceptCallAndOpenApp.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+        Bundle openIncomingCallScreenInitialProps = new Bundle();
+        openIncomingCallScreenInitialProps.putBoolean("isIncomingCall", true);
+        acceptCallAndOpenApp.putExtras(openIncomingCallScreenInitialProps);
 
         PendingIntent pendingDismissBannerIntent = PendingIntent.getService(getApplicationContext(), 0,
                 dismissBannerIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        PendingIntent acceptCallPendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, 
-                acceptCallIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent acceptCallPendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, acceptCallAndOpenApp, PendingIntent.FLAG_UPDATE_CURRENT);
 
-        PendingIntent openIncomingCallScreenPendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, 
-                openAppIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingIntent openIncomingCallScreenPendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, openIncomingCallScreen, PendingIntent.FLAG_UPDATE_CURRENT);
 
         if (action.equals(START_CALL_BANNER)) {
             HashMap<String, String> payload = (HashMap<String, String>) intent.getSerializableExtra(ACTION_PAYLOAD_KEY);
@@ -105,28 +106,6 @@ public class EyrCallBannerDisplayService extends Service {
                 reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
                         .emit("CALL_IS_DECLINED", null);
             }
-        }
-
-        if (action.equals(ACCEPT_CALL)) {
-            Intent acceptCallAndOpenApp = new Intent(this, getMainActivityClass());
-            acceptCallAndOpenApp.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-            Bundle acceptCallAndOpenAppInitialProps = new Bundle();
-            acceptCallAndOpenAppInitialProps.putBoolean("isCallAccepted", true);
-            acceptCallAndOpenApp.putExtras(acceptCallAndOpenAppInitialProps);
-            startActivity(acceptCallAndOpenApp);
-            // remove call notification
-            stopForeground(true);
-        }
-
-        if (action.equals(OPEN_INCOMING_CALL)) {
-            Intent openIncomingCallScreen = new Intent(this, getMainActivityClass());
-            openIncomingCallScreen.addFlags(Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
-            Bundle openIncomingCallScreenInitialProps = new Bundle();
-            openIncomingCallScreenInitialProps.putBoolean("isIncomingCall", true);
-            openIncomingCallScreen.putExtras(openIncomingCallScreenInitialProps);
-            startActivity(openIncomingCallScreen);
-            // remove call notification
-            stopForeground(true);
         }
 
         return START_NOT_STICKY;
