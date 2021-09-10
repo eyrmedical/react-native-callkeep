7a20c11da4ba9b3267faafe05774cd28a22b99b7
Nguyen Luong <luongnguyenkhoinguyen14@gmail.com>
2021-09-10 11:07:30 +0300

    Fixed an error in action.js file

--- actions.js
+++ actions.js
@@ -10,11 +10,11 @@ const EYRCallKeepDidLoadWithEvents = 'EYRCallKeepDidLoadWithEvents';
 
 const isIOS = Platform.OS === 'ios';
 
-const answerCall = handler => eventEmitter.addListener(EYRCallKeepPerformAnswerCallAction, , handler);
+const answerCall = handler => eventEmitter.addListener(EYRCallKeepPerformAnswerCallAction ,handler);
 
-const endCall = handler => eventEmitter.addListener(EYRCallKeepPerformEndCallAction, , handler);
+const endCall = handler => eventEmitter.addListener(EYRCallKeepPerformEndCallAction, handler);
 
-const didPerformSetMutedCallAction = handler => eventEmitter.addListener(EYRCallKeepDidPerformSetMutedCallAction, , handler);
+const didPerformSetMutedCallAction = handler => eventEmitter.addListener(EYRCallKeepDidPerformSetMutedCallAction, handler);
 
 const didLoadWithEvents = handler => eventEmitter.addListener(EYRCallKeepDidLoadWithEvents, handler);
 
