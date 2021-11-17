aa395739a6f14685cacb5f3ca7979b903823dfc6
mdrakus <mikhail.drakus@eyr.md>
2021-11-17 19:27:34 +0200

    Fix missing symbols

--- android/src/main/java/com/eyr/callkeep/Utils.java
+++ android/src/main/java/com/eyr/callkeep/Utils.java
@@ -13,6 +13,7 @@ import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -199,7 +200,7 @@ public class Utils {
         .build();
 
       channel.setImportance(NotificationManager.IMPORTANCE_HIGH);
-      channel.setSound(uri, audioAttributes);
+      channel.setSound(uri, attrs);
       channel.enableVibration(false);
       channel.enableLights(false);
       channel.setBypassDnd(true);
